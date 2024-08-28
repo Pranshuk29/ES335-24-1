0622a6fd41e7179cd5c7e82a4d65187d98a2ae10
Pranshuk29 <145792354+Pranshuk29@users.noreply.github.com>
2024-08-28 06:59:08 +0530

    Update utils.py

--- tree/utils.py
+++ tree/utils.py
@@ -4,74 +4,98 @@ There is no restriction on following the below template, these fucntions are her
 """
 
 import pandas as pd
+import numpy as np
 
 def one_hot_encoding(X: pd.DataFrame) -> pd.DataFrame:
     """
     Function to perform one hot encoding on the input data
     """
-
-    pass
+    return pd.get_dummies(X, drop_first=True)
+    
 
 def check_ifreal(y: pd.Series) -> bool:
     """
     Function to check if the given series has real or discrete values
     """
-
-    pass
+    # Assuming that if a series has only integer values, it is discrete; otherwise, it is real
+    return pd.api.types.is_float_dtype(y) or pd.api.types.is_numeric_dtype(y)
 
 
 def entropy(Y: pd.Series) -> float:
     """
     Function to calculate the entropy
     """
+    value_counts = Y.value_counts(normalize=True)
+    return -np.sum(value_counts * np.log2(value_counts + 1e-9))  # Adding epsilon to avoid log(0)
 
-    pass
 
 
 def gini_index(Y: pd.Series) -> float:
     """
     Function to calculate the gini index
     """
+    value_counts = Y.value_counts(normalize=True)
+    return 1 - np.sum(value_counts ** 2)
 
-    pass
 
 
 def information_gain(Y: pd.Series, attr: pd.Series, criterion: str) -> float:
     """
     Function to calculate the information gain using criterion (entropy, gini index or MSE)
     """
+    if criterion == 'entropy':
+        base_entropy = entropy(Y)
+        weighted_entropy = 0
+        for value in attr.unique():
+            subset_Y = Y[attr == value]
+            weighted_entropy += (len(subset_Y) / len(Y)) * entropy(subset_Y)
+        return base_entropy - weighted_entropy
 
-    pass
+    elif criterion == 'gini':
+        base_gini = gini_index(Y)
+        weighted_gini = 0
+        for value in attr.unique():
+            subset_Y = Y[attr == value]
+            weighted_gini += (len(subset_Y) / len(Y)) * gini_index(subset_Y)
+        return base_gini - weighted_gini
+
+    elif criterion == 'MSE':
+        base_mse = np.mean((Y - Y.mean()) ** 2)
+        weighted_mse = 0
+        for value in attr.unique():
+            subset_Y = Y[attr == value]
+            weighted_mse += (len(subset_Y) / len(Y)) * np.mean((subset_Y - subset_Y.mean()) ** 2)
+        return base_mse - weighted_mse
+
+    else:
+        raise ValueError(f"Unknown criterion: {criterion}")
 
 
-def opt_split_attribute(X: pd.DataFrame, y: pd.Series, criterion, features: pd.Series):
+
+def opt_split_attribute(X: pd.DataFrame, y: pd.Series, criterion: str, features: pd.Series):
     """
     Function to find the optimal attribute to split about.
-    If needed you can split this function into 2, one for discrete and one for real valued features.
-    You can also change the parameters of this function according to your implementation.
-
-    features: pd.Series is a list of all the attributes we have to split upon
-
-    return: attribute to split upon
     """
+    best_gain = -float('inf')
+    best_feature = None
 
-    # According to wheather the features are real or discrete valued and the criterion, find the attribute from the features series with the maximum information gain (entropy or varinace based on the type of output) or minimum gini index (discrete output).
+    for feature in features:
+        gain = information_gain(y, X[feature], criterion)
+        if gain > best_gain:
+            best_gain = gain
+            best_feature = feature
 
-    pass
+    return best_feature
 
 
 def split_data(X: pd.DataFrame, y: pd.Series, attribute, value):
     """
-    Funtion to split the data according to an attribute.
-    If needed you can split this function into 2, one for discrete and one for real valued features.
-    You can also change the parameters of this function according to your implementation.
-
-    attribute: attribute/feature to split upon
-    value: value of that attribute to split upon
-
-    return: splitted data(Input and output)
+    Function to split the data according to an attribute.
     """
+    if pd.api.types.is_numeric_dtype(X[attribute]):
+        mask = X[attribute] <= value
+        return X[mask], y[mask], X[~mask], y[~mask]
+    else:
+        mask = X[attribute] == value
+        return X[mask], y[mask], X[~mask], y[~mask]
 
-    # Split the data based on a particular value of a particular attribute. You may use masking as a tool to split the data.
-
-    pass
